cff65a2c2fc459030d46af150a60ea2038deca9f
Amit Kumar <amit.nokhwal3@gmail.com>
2026-05-18 17:47:47 +0530

    the adding multiply fil

---
+++ multiply.java
@@ -0,0 +1,10 @@
+public class multiply {
+    public static void multiply(int a , int b){
+        int result = a* b;
+        System.out.println("REsult :"+ result);
+    }
+    public static void main(String[] args) {
+        multiply(3949,38);
+  
+    }
+}
