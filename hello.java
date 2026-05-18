663db108993eb5a297e7d4fe52b207f81b8ef414
Amit Kumar <amit.nokhwal3@gmail.com>
2026-05-18 13:40:43 +0530

    initial commit

---
+++ hello.java
@@ -0,0 +1,15 @@
+public class hello {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+        System.out.println("Welcome to Java Programming");
+
+        int a = 10;
+        int b = 20;
+
+        int sum = a + b;
+
+        System.out.println("Sum = " + sum);
+    }
+}
